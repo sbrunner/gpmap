47c66df6ad7b7276dd3a744959cac5b6c6612ebf
Stéphane Brunner <courriel@stephane-brunner.ch>
2012-07-29 08:56:52 +0200

    add example

--- gpmap/static/js/Proj/GPPrint.js
+++ gpmap/static/js/Proj/GPPrint.js
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2012 Stéphane Brunner
  *
- * Map is free software: you can redistribute it and/or modify
+ * GPMap is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Map is distributed in the hope that it will be useful,
+ * GPMap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Map.  If not, see <http://www.gnu.org/licenses/>.
+ * along with CPMap.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -38,7 +38,12 @@ Ext.namespace("gpmap.plugins");
  *      new gxp.Viewer({
  *          ...
  *          tools: [{
- *              ptype: 'gpmap_gpprint'
+ *              ptype: "gpmap_gpprint",
+ *              actionTarget: "center.tbar",
+ *              outputTarget: "main",
+ *              toggleGroup: "maptools",
+ *              printURL: "${request.route_url('printproxy')}",
+ *              scale: true
  *          }]
  *          ...
  *      });
